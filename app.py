00348cffc38ab8c44c76be0a616a8bd5d865d077
darthshittious <varunj26012001@gmail.com>
2021-02-19 18:00:15 +0530

    Return parsed date in response

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def hello_world():
 
 @app.route('/gestation')
 async def get_gestation_age():
+    """Return the Expected Delivery Date and the gestation age (in weeks.days format) given a date string corresponding
+    to the last menstrual period.
+    """
     if (dt := request.args.get('lmp')) is None:
         abort(400)
     try:
@@ -19,18 +22,16 @@ async def get_gestation_age():
     except ValueError:
         return 'bad request', 400
     if dt._past is False:
+        # pdt converts all dates to future when year isn't specified. We assume this was the case when the year is up
+        # by one
         if dt.dt.date().year - date.today().year == 1:
-            print(date.today().year)
             dt.dt = dt.dt.replace(year=date.today().year)
-            print(dt.dt)
         else:
-            print(dt.dt)
             return {'message': 'This date is in the future.', 'parsed_date': dt.dt.strftime('%d %b %Y')}, 400
-    print(dt.dt)
     edd = (dt.dt.date() + timedelta(days=7 * 41)).strftime('%d %b %Y')
 
     gest_age = divmod((date.today() - dt.dt.date() - timedelta(days=7)).days, 7)
-    return {'edd': edd, 'gestation_age': gest_age[0] + gest_age[1] * 0.1}
+    return {'lmp': dt.dt.date().strftime('%d %b %Y'), 'edd': edd, 'gestation_age': gest_age[0] + gest_age[1] * 0.1}
 
 
 if __name__ == '__main__':
