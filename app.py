6026ba469893d46ff73c9542297d6314bb7f17a8
darthshittious <varunj26012001@gmail.com>
2021-02-19 16:20:57 +0530

    Major fixes

--- app.py
+++ app.py
@@ -12,18 +12,25 @@ def hello_world():
 
 @app.route('/gestation')
 async def get_gestation_age():
-    if dt := request.args.get('lmp') is None:
+    if (dt := request.args.get('lmp')) is None:
         abort(400)
     try:
         dt = Time(dt)
     except ValueError:
         return 'bad request', 400
     if dt._past is False:
-        return 'This date is in the future.', 400
-    w, d = divmod((dt.dt.date() + timedelta(days=7 * 41)).days, 7)
+        if dt.dt.date().year - date.today().year == 1:
+            print(date.today().year)
+            dt.dt = dt.dt.replace(year=date.today().year)
+            print(dt.dt)
+        else:
+            print(dt.dt)
+            return {'message': 'This date is in the future.', 'parsed_date': dt.dt.strftime('%d %b %Y')}, 400
+    print(dt.dt)
+    edd = (dt.dt.date() + timedelta(days=7 * 41)).strftime('%d %b %Y')
 
-    gest_age = divmod((date.today() - dt.dt.date() + timedelta(days=7)).days, 7)
-    return {'edd': f'{w}.{d}', 'gestation_age': f'{gest_age[0]}.{gest_age[1]}'}
+    gest_age = divmod((date.today() - dt.dt.date() - timedelta(days=7)).days, 7)
+    return {'edd': edd, 'gestation_age': gest_age[0] + gest_age[1] * 0.1}
 
 
 if __name__ == '__main__':
