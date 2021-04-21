d1aa7f5be5772460f1d5a157439fbb30afca9367
darthshittious <varunj26012001@gmail.com>
2021-04-21 16:15:23 +0200

    Update Discord tag

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ async def close_pool():
 
 @app.route('/')
 def hello_world():
-    return '<samp>VJ#0001 on Discord. Feature requests rarely accepted.</samp>'
+    return '<samp>VJ#5945 on Discord. Feature requests rarely accepted.</samp>'
 
 
 @app.route('/gestation')
